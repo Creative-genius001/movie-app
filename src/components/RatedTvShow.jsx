4c4a5c06de1b4309c9df1019bf4813d53ed9b422
Ovie Igho <John.Henry5923@gmail.com>
2022-07-09 07:51:48 -0700

    update

--- src/components/RatedTvShow.jsx
+++ src/components/RatedTvShow.jsx
@@ -6,33 +6,31 @@ import MovieCards from "./MovieCards";
 const RatedTvShow = () => {
 	const [ratedShow, setRatedShow] = useState([]);
 	useEffect(() => {
-		getRatedShow();
+		const getRatedShow = async () => {
+			const check =
+				localStorage.getItem("ratedShow");
+
+			if (check) {
+				setRatedShow(JSON.parse(check));
+			} else {
+				const response = await axios
+					.get(
+						`https://api.themoviedb.org/3/tv/top_rated?api_key=0a82a71f7db762d5f3249e80ca6bc5db&language=en-US&page=1`,
+					)
+					.catch((err) => console.error(err));
+
+				let res = response.data.results;
+
+				localStorage.setItem(
+					"ratedShow",
+					JSON.stringify(res),
+				);
+
+				setRatedShow(res);
+				console.log(ratedShow);
+			}
+		};
 	}, []);
-
-	const getRatedShow = async () => {
-		const check =
-			localStorage.getItem("ratedShow");
-
-		if (check) {
-			setRatedShow(JSON.parse(check));
-		} else {
-			const response = await axios
-				.get(
-					`https://api.themoviedb.org/3/tv/top_rated?api_key=0a82a71f7db762d5f3249e80ca6bc5db&language=en-US&page=1`,
-				)
-				.catch((err) => console.error(err));
-
-			let res = response.data.results;
-
-			localStorage.setItem(
-				"ratedShow",
-				JSON.stringify(res),
-			);
-
-			setRatedShow(res);
-			console.log(ratedShow);
-		}
-	};
 
 	return (
 		<div className="trending w-auto my-4 mx-auto">
